77ce1bd807abcaf5cbbb1ced52b9050a32ba64e3
Casey Lee <casey.lee.u20@gmail.com>
2022-01-31 23:41:25 -0500

    Updated test_point_on_line to test for slope_2points instead of create_line

--- test_point_on_line.py
+++ test_point_on_line.py
@@ -2,13 +2,18 @@
 
 import pytest
 
-def test_create_line():
-	from point_on_line import create_line
-	answer = create_line([1,1],[2,2])
-	assert answer == 1
+#def test_create_line():
+#	from point_on_line import create_line
+#	answer = create_line([1,1],[2,2])
+#	assert answer == 1
+
+def test_slope_2points():
+    from point_on_line import slope_2points
+    answer = slope_2points((-3,4),(5,-6))
+    assert answer == -5/4
 
 
 def test_output_y():
 	from point_on_line import output_y
-	answer = output_y(3)
-	assert answer == 3
+	answer = output_y(-5,2,(3,-4))
+	assert answer == -20
