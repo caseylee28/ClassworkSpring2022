493c378edfef746cb2bdd50e66fbd86fabbc0a0b
Casey Lee <casey.lee.u20@gmail.com>
2022-01-31 22:30:55 -0500

    Added function the connects both points

--- point_on_line.py
+++ point_on_line.py
@@ -3,21 +3,21 @@ def input_point():
     point_two = input("Coordinate Point 2: ")
     return point_one, point_two
 
-#def connect_points():
-#    connect_points = (eval(point_one),eval(point_two))
-#    return connect_points
+
+def connect_points(point1,point2):
+    connect_2points = (eval(point1),eval(point2))
+    return connect_2points
     
     
 def main():
     point1,point2 = input_point()
     print(point1)
     print(point2)
-
-#    two_points = input_point()
-#    print(two_points)
-#    print(two_points[0])
-#    print(two_points[1])
-#    print(two_points[1][1])
+    two_points = connect_points(point1,point2)
+    print(two_points)
+    print(two_points[0])
+    print(two_points[1])
+    print(two_points[1][1])
 
 
 if __name__ == "__main__":
